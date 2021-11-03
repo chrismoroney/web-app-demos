67f718b905a1b20b3d676ebd226260c81736fe66
Chris Moroney <christopher.moroney1@gmail.com>
2021-11-03 11:07:41 -0700

    Upload project example

---
+++ React/first-app/src/components/NavBar.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import CustomBtn from './CustomBtn'
+import logo from '../logo.svg'
+import logoMobile from '../logoMobile.svg'
+import {Toolbar, Typography} from '@material-ui/core'
+import {makeStyles} from "@material-ui/core/styles"; 
+
+const styles = makeStyles({
+    bar:{
+        paddingTop: "1.15rem",
+        backgroundColor: "#fff",
+    },
+    logo: {
+        width: "15%", 
+    },
+    logoMobile:{
+        width: "100%", 
+        display: "none", 
+    },
+    menuItem: {
+        cursor: "pointer", 
+        flexGrow: 1,
+        "&:hover": {
+            color:  "#4f25c8"
+        }
+    }
+})
+
+function NavBar() {
+    const classes = styles()
+    return (
+            <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>   
+                <img src={logo} className={classes.logo}/> 
+                <img src={logoMobile} className={classes.logoMobile}/> 
+                <Typography variant="h6" className={classes.menuItem}>
+                   About
+                </Typography>
+                <Typography variant="h6" className={classes.menuItem}>
+                    Blog
+                </Typography>
+                <Typography variant="h6" className={classes.menuItem}>
+                    Careers
+                </Typography>
+                <Typography variant="h6" className={classes.menuItem}>
+                    Demos 
+                </Typography>
+                <Typography variant="h6" className={classes.menuItem}>
+                    Contact Us 
+                </Typography>
+                <CustomBtn txt="Trial Our Product"/>
+            </Toolbar>
+    )
+}
+
+export default NavBar
